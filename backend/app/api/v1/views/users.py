3681b233a7ff4475635add8e8dc2d56ea506a877
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-07-28 23:29:33 +0200

    implemented CRUD on users endpoints

--- backend/app/api/v1/views/users.py
+++ backend/app/api/v1/views/users.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, session, abort, jsonify
 from email_validator import validate_email, EmailNotValidError
 from app.api.v1.views import app_views
 from app.models import User
+from app.schemas import user_schema, users_schema
 from app import db, bcrypt
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
@@ -39,7 +40,7 @@ def create_user():
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": "Failed to create a user", "details": str(e)}), 500
-    return jsonify({"msg": "created successfully"}), 201
+    return user_schema.jsonify(user), 201
 
 @app_views.route("users/auth", methods=["POST"])
 def auth():
@@ -50,8 +51,10 @@ def auth():
         if field not in data:
             return jsonify({"error": f"Missing {field}"}), 400
     user = User.query.filter_by(email=data["email"]).first()
-    if not user and bcrypt.check_password_hash(user.password, data["password"]):
-        return jsonify({"error": "check email and password"}), 400
+    if not user:
+        return jsonify({"error": "Invalid email"}), 400
+    if not bcrypt.check_password_hash(user.password, data["password"]):
+        return jsonify({"error": "Invalid password"}), 400
     access_token = create_access_token(identity=user.id)
     return jsonify({
         "access_token": access_token,
@@ -61,21 +64,40 @@ def auth():
 @app_views.route("/users/<uuid:user_id>")
 @jwt_required()
 def get_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "user not found"}), 404
     curr_user_id = get_jwt_identity()
     if str(curr_user_id) != str(user_id):
-        return jsonify({"error": "unauthorized"}), 401
-    return jsonify(user.to_dict()), 200
+        abort(403)
+    user = User.query.get(user_id)
+    if not user:
+        abort(404)
+    return user_schema.jsonify(user), 200
 
 @app_views.route("/users/<uuid:user_id>", methods=["PUT", "DELETE"])
 @jwt_required()
 def update_user(user_id):
     curr_user_id = get_jwt_identity()
     if str(curr_user_id) != str(user_id):
-        return jsonify({"error": "unauthorized"}), 401
+        abort(403)
     user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "user not found"}), 404
+    if request.method == "PUT":
+        if not request.is_json:
+            abort(400)
+        data = request.get_json()
+        try:
+            for key, value in data.items():
+                setattr(user, key, value)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": "could not update user info", "details": str(e)}), 500
+        return user_schema.jsonify(user), 200
+    elif request.method == "DELETE":
+        try:
+            db.session.delete(user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": "could not delete the user", "details": str(e)}), 500
+        return jsonify({}), 200
+
     
