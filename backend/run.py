bed96f1a7ed12d9bf5df7ce87968a67322c50812
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-03-20 15:35:12 +0200

    manually refresh the token

--- backend/run.py
+++ backend/run.py
@@ -1,17 +1,8 @@
 """
 Flask application entry point.
 
-This module imports and initializes the Flask application, registers error handlers, 
-and ensures the database tables are created before running the server. 
-
-It also includes a function to refresh JWTs automatically when their expiration time 
-is near, helping maintain user sessions seamlessly.
+This module imports and initializes the Flask application, registers error handlers. 
 """
-from datetime import datetime, timedelta, timezone
-from flask_jwt_extended import(get_jwt,
-                            get_jwt_identity,
-                            create_access_token,
-                            set_access_cookies)
 from app import create_app, db 
 from app.error_handlers import register_error_handlers
 from app.models.user import User
@@ -24,19 +15,6 @@ from app.models.notification import Notification
 app = create_app()
 
 register_error_handlers(app)
-
-@app.after_request
-def refresh_exp_jwts(resp):
-    try:
-        exp_timestamp = get_jwt()["exp"]
-        now = datetime.now(timezone.utc)
-        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
-        if target_timestamp > exp_timestamp:
-            access_token = create_access_token(identity=get_jwt_identity())
-            set_access_cookies(resp, access_token)
-        return resp
-    except (RuntimeError, KeyError):
-        return resp
 
 if __name__== "__main__":
     with app.app_context():
