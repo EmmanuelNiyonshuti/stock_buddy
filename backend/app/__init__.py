682f23ceab287f6ccd2b112524e13cffda1cff27
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-03-28 11:09:49 +0200

    modified __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -33,6 +33,7 @@ def create_app(config_class=Config, testing=False):
     if testing:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         app.config["TESTING"] = True
+        app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
         app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
         app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
         app.config["JWT_ACCESS_COOKIE_NAME"] = "access_cookie"
